005e45c5c1bf9fd41236e058ae22ba36294fc5cf
Larry NeJame <lnejame@nebids.com>
2012-07-17 16:45:49 -0400

    changed some things

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -44,7 +44,7 @@ class CompaniesController < ApplicationController
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to user_steps_path }
+        format.html { redirect_to @company, notice: 'Company was successfully created.' }
         format.json { render json: @company, status: :created, location: @company }
       else
         format.html { render action: "new" }
--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -3,6 +3,13 @@ class UserStepsController < ApplicationController
   steps :company, :project, :comments
   
   def show
+    @company = Company.new
     render_wizard
+  end
+  
+  def update
+    @company = Company.find(params[:id])
+    @company.attributes = params[:company]
+    render_wizard @company
   end
 end
